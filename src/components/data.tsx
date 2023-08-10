292e8905f767b07f7d9f699dcd234af15c6633fa
prtkjakhar <prtkjakhar@gmail.com>
2023-08-10 16:36:16 +0530

    sentences showing

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -8,11 +8,25 @@ import {
   Edit,
   SimpleForm,
   TextInput,
-  useRecordContext
+  useRecordContext,
+  Filter,
+  useNotify,
 } from 'react-admin';
+import React from 'react';
+import Popup from './popup';
+
+const DataFilter = (props: any) => {
+  return (
+    <Filter {...props}>
+      <TextInput label="Search Source" source="source" alwaysOn />
+      <TextInput label="Search Translation" source="translation" alwaysOn />
+      <TextInput label="Search By Use" source="use" alwaysOn />
+    </Filter>
+  );
+};
 
 export const DataList = () => (
-  <List title="Dict Augmentation of Azure translation">
+  <List title="Dict Augmentation of Azure translation" filters={<DataFilter />}>
     <Datagrid rowClick="edit">
       <TextField source="id" />
       <TextField source="source" />
@@ -25,26 +39,134 @@ export const DataList = () => (
 const DataTitle = () => {
   const record = useRecordContext();
   return <span>{record ? `"${record.source}"` : 'source'}</span>;
-}
+};
 
-export const DataEdit = () => (
-  <Edit title={<DataTitle/>}>
-    <SimpleForm>
-      <TextInput source="id" disabled/>
-      <BooleanInput source="use" />
-      <TextInput source="source" />
-      <TextInput source="translation" />
-    </SimpleForm>
-  </Edit>
-);
+export const DataEdit = () => {
+  const [modalContent, setModalContent] = React.useState(null);
+  const notify = useNotify();
 
-export const DataCreate = () => (
-  <Create title="Create Data">
-    <SimpleForm>
-      {/* <TextInput source="id" /> */}
-      <BooleanInput source="use" />
-      <TextInput source="source" />
-      <TextInput source="translation" />
-    </SimpleForm>
-  </Create>
-);
+  const handleEditSubmit = (formData: any) => {
+    // Make the API call to update data in the table
+    fetch(`${process.env.NEXT_PUBLIC_BFF_URL}/translationdictionary/${formData.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        source: formData.source,
+        translation: formData.translation,
+        use: formData.use,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to update data');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the API response for updation
+        notify('Data updated successfully');
+
+        // Make the second API call
+        return fetch('http://aitools.chatwithpdf.aitools.samagra.io/data_generation/dictionary_aug/remote/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            source: formData.source,
+            translation: formData.translation,
+          }),
+        });
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the second API response
+        const content = JSON.parse(data.choices[0].message.content);
+        setModalContent(content);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error('Error:', error);
+        notify('Error updating data');
+      });
+  };
+
+  return (
+    <Edit title={<DataTitle />}>
+      <SimpleForm onSubmit={handleEditSubmit}>
+        <TextInput source="id" disabled />
+        <TextInput source="source" />
+        <TextInput source="translation" />
+        <BooleanInput source="use" />
+        {modalContent && <Popup postResponseContent={modalContent} />}
+      </SimpleForm>
+    </Edit>
+  );
+};
+
+export const DataCreate = () => {
+  const [modalContent, setModalContent] = React.useState(null);
+  const notify = useNotify();
+
+  const handleFormSubmit = (formData: any) => {
+    // Make the API call to create data in the table
+    fetch(`${process.env.NEXT_PUBLIC_BFF_URL}/translationdictionary`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        source: formData.source,
+        translation: formData.translation,
+        use: formData.use,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to create data');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the API response for creation
+        notify('Data created successfully');
+
+        // Make the second API call
+        return fetch('http://aitools.chatwithpdf.aitools.samagra.io/data_generation/dictionary_aug/remote/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            source: formData.source,
+            translation: formData.translation,
+          }),
+        });
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the second API response
+        const content = JSON.parse(data.choices[0].message.content);
+        setModalContent(content);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error('Error:', error);
+        notify('Error creating data');
+      });
+  };
+
+  return (
+    <Create title="Create Data">
+      <SimpleForm onSubmit={handleFormSubmit}>
+        {/* <TextInput source="id" /> */}
+        <TextInput source="source" />
+        <TextInput source="translation" />
+        <BooleanInput source="use" />
+        {modalContent && <Popup postResponseContent={modalContent} />}
+      </SimpleForm>
+    </Create>
+  );
+};
---
+++ src/components/popup.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+
+function Popup(props: any) {
+  const [openDialog, setOpenDialog] = useState(props?.postResponseContent ? true : false);
+
+  return (
+    <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+        <DialogTitle>Translations</DialogTitle>
+        <DialogContent>
+        {props?.postResponseContent?.map((line: any, index: number) => (
+            <DialogContentText key={index}>
+              {line[0]} - {line[1]}
+            </DialogContentText>
+          ))}
+        </DialogContent>
+      </Dialog>
+  )
+}
+
+export default Popup
